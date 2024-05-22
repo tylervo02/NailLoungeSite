235b0ec37614ed1476f94d79756c9e44717877c1
Tyler Vo <tylervo02@yahoo.com>
2024-05-21 22:58:45 -0700

    First Pages

---
+++ nail-lounge-site/src/Pages/HomePage.js
@@ -0,0 +1,9 @@
+import React, {Component, useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+function HomePage() {
+    return (
+        <h1>101 Nail Lounge</h1>
+        
+    );
+}
